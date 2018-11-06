ae5266ce849580f90440eca3ee92c10005b0f724
saumil.shah <saumil.shah@IN-IT1240>
2018-11-06 13:03:47 +0530

    Changes

--- WebsiteTest.ps1
+++ WebsiteTest.ps1
@@ -46,58 +46,7 @@ Configuration WebsiteTest {
         Name   = "AD-Domain-Services"
     }
 
-    # Create AD Domain specified in HADCServerConfigData.
-    xADDomain FirstDC
-    {
-        # Name of the remote domain. If no parent name is specified, this is the fully qualified domain name for the first domain in the forest.
-        DomainName                    = $DomainName
-        # Credentials used to query for domain existence.
-        DomainAdministratorCredential = $DomainCreds
-        # Password for the administrator account when the computer is started in Safe Mode.
-        SafemodeAdministratorPassword = $SafeModeDomainCreds
-        # Specifies the fully qualified, non-Universal Naming Convention (UNC) path to a directory on a fixed disk of the local computer that contains the domain database (optional).
-        DatabasePath                  = "C:\NTDS"
-        # Specifies the fully qualified, non-UNC path to a directory on a fixed disk of the local computer where the log file for this operation will be written (optional).
-        LogPath                       = "C:\NTDS"
-        # Specifies the fully qualified, non-UNC path to a directory on a fixed disk of the local computer where the Sysvol file will be written. (optional)
-        SysvolPath                    = "C:\SYSVOL"
-        # DependsOn specifies which resources depend on other resources, and the LCM ensures that they are applied in the correct order, regardless of the order in which resource instances are defined.
-        DependsOn                     = "[WindowsFeature]ADDSInstall"
-    }
-
-    # Wait until AD Domain is created.
-    xWaitForADDomain DomainWait
-    {
-        DomainName           = $DomainName
-        DomainUserCredential = $DomainCreds
-        # Maximum number of retries to check for the domain's existence.
-        RetryCount           = $Node.RetryCount
-        # Interval to check for the domain's existence.
-        RetryIntervalSec     = $Node.RetryIntervalSec
-        DependsOn            = "[xADDomain]FirstDC"
-    }
-
-    # Enable Recycle Bin.
-    xADRecycleBin RecycleBin
-    {
-        # Credential with Enterprise Administrator rights to the forest.
-        EnterpriseAdministratorCredential = $DomainCreds
-        # Fully qualified domain name of forest to enable Active Directory Recycle Bin.
-        ForestFQDN                        = $DomainName
-        DependsOn                         = "[xWaitForADDomain]DomainWait"
-    }
-
-    # Create AD User "Test.User".
-    xADUser ADUser
-    {
-        DomainName                    = $DomainName
-        DomainAdministratorCredential = $DomainCreds
-        UserName                      = "Test.User"
-        Password                      = $SafeModeCredentials
-        Ensure                        = "Present"
-        DependsOn                     = "[xWaitForADDomain]DomainWait"
-    }
-
+   
   }
 
 }
